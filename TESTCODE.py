f6800d04be51c54dfe2acffefa6d06004f434987
NguyenLeTungLam <lam2007tb@gmail.com>
2026-05-20 09:18:57 +0700

    bt

--- TESTCODE.py
+++ TESTCODE.py
@@ -1,6 +1,10 @@
-tup = ( 1 , 3 , 2 , 2, "fdfsfsf")
-lst = list(tup)
-lst[0]= 100
-tup = tuple(lst)
-print(tup)
-
+def merge(arr1 , arr2 ):
+    res = []
+    if arr1[0] < arr1[-1]:
+        i = j = 0
+        while i < len(arr1) and j < len(arr2):
+            if arr1[i] < arr2[j]:
+                res.append(arr1[i])
+                i += 1
+    else:
+        while i < len(arr1) and j < arr2
